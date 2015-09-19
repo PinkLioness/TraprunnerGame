e4acf511b5045e1437bddd3910a5eb70e89a8ba3
PinkLioness <joao_albertoni+github@hotmail.com>
2015-09-19 18:28:23 -0300

    Demo code and some of the main menu code

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ var GAME = GAME || {};
 GAME.main = {
 	mainMenu:function(){
 		GAME.interface.clearText();
-		GAME.interface.addText("A few things will be here later, like a maze generation selector, a sex selector for your char, etc.");
+		GAME.interface.addText("Congratulations, you've been selected to compete in the TrapRunner gameshow, brand new toasters and sleek roadsters await you, etc etc etc. We need a decent intro");
+		GAME.interface.addText('Your name: <input type="text" id="charName">');
+		GAME.interface.addText('Your gender: <select id="sexType"><option value="1">Male</option><option value="2">Female</option><option value="3">Herm (hard mode)</option><option value="4">Shemale</option><option value="5">Cuntboy</option></select>');
+		GAME.interface.addText('Maze type: <select id="mazeType"><option value="1">Long and winding</option><option value="2">Random growth</option><option value="3">Long and winding + Random</option><option value="4">Straightened</option></select>');
+		GAME.interface.addText('Maze size (5 to 20, bigger is harder): <input type="number" id="mazeSize" value="10" min="5" max="20">');
 
-		GAME.interface.drawButtons({text:'New game', buttonFunction:GAME.main.newGame.sexQuestion});
+		GAME.interface.drawButtons({text:'New game', buttonFunction:GAME.main.newGame.start});
 	},
 	newGame:{ // Contains all functions related to starting a new game.
 		sexQuestion:function(){
@@ -22,8 +26,32 @@ GAME.main = {
 			GAME.interface.drawButtons({text:'Male', buttonFunction:GAME.main.newGame.choseMale}, {text:'Female', buttonFunction:GAME.main.newGame.choseFemale});*/
 		},
 		start:function(){
-			// TODO: Set location to town
-			// TODO: Add starting equipment?
+			/*
+			get data from menu
+			if all input is ok
+				SANITIZE PLAYER NAME FOR EVIL SCRIPTS
+				use data to generate maze and create player
+			else
+				warn player
+			*/
+			
+			var name = document.getElementById('charName').value.replace(/</, '&lt;');
+			if(name == ''){
+				name = 'Unnamed';
+			}
+			
+			var gender = document.getElementById('sexType').value;
+			
+			var mazeType = Number(document.getElementById('mazeType').value);
+			
+			var mazeSize = Number(document.getElementById('mazeSize').value);
+			
+			
+			GAME.interface.mazeArea.innerHTML = '';
+			var x = drawMaze(GrowingTreeMazeGenerator(mazeSize, mazeSize, mazeType));
+			GAME.player.reset();
+			x[0][0].style.backgroundColor = 'red';
+			GAME.interface.statContainers['lust'].updateValue(70);
 		}
 	}
 };
